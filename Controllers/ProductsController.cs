d0def9d0604e25c4ee36f1b1f6acbe8c6114d143
Kenneth Coleman <200403248@student.georgianc.on.ca>
2020-01-16 21:27:13 -0500

    Add project files.

---
+++ Controllers/ProductsController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lab2.Controllers
+{
+    public class ProductsController : Controller
+    {
+        // GET: Products
+        public ActionResult Index()
+        {
+            List<String> products = new List<string> { "Laptop", "Tablet", "Desktop" };
+
+            ViewData.Add("Products", products);
+
+            return View();
+        }
+
+        public ActionResult Details(string product)
+        {         
+            ViewBag.Message = "You selected: " + product;
+
+            return View();
+        }
+    }
+}
